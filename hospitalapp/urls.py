f1d701ae27a03e839256b0872d49ead0d5949097
Fr3aky-b <wizkie62@gmail.com>
2025-02-25 13:08:20 +0300

    second

--- hospitalapp/urls.py
+++ hospitalapp/urls.py
@@ -5,8 +5,12 @@ from hospitalapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index,name='home'),
-    path('service/', views.services,name='services'),
-    path('starter/', views.starter,name='starter')
+    path('home/', views.index,name='home'),
+    path('service/', views.services,name='service'),
+    path('starter/', views.starter,name='starter'),
+    path('about/', views.about,name='about'),
+    path('services/', views.services,name='services'),
+    path('departments/', views.departments,name='departments'),
+    path('doctors/', views.doctors,name='doctors')
 
 ]
--- hospitalapp/views.py
+++ hospitalapp/views.py
@@ -7,8 +7,22 @@ def index(request):
   return render(request,'index.html')
 
 
-def services(request):
+def service(request):
   return render(request,'service-details.html')
 
 def starter(request):
   return render(request,'starter-page.html')
+
+
+def about(request):
+  return render(request,'about.html')
+
+
+def services(request):
+  return render(request,'services.html')
+
+def departments(request):
+  return render(request,'departments.html')
+
+def doctors(request):
+  return render(request,'doctors.html')
